bed3f3149f0dea146bb14e79574239e25c120ef3
david muhlberger <david.muhlberger@gmail.com>
2015-06-23 12:22:30 +0200

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(email: "fake_user_1@fakemail.com", password: "fakefake")
+User.create(email: "fake_user_2@fakemail.com", password: "fakefake")
+
+Flat.create(user_id: 2, title: "Flat in Paris", description: "Nice flat in Paris", address_numberstreet: "1 rue du Cygne", address_zip: 75001, address_city: "Paris",address_country: "France",night_price: 100,capacity: 2)
+Flat.create(user_id: 3, title: "Flat in Villejuif", description: "Nice flat in Villejuif", address_numberstreet: "1 rue du Cygne", address_zip: 75001, address_city: "Paris",address_country: "France",night_price: 50,capacity: 1)
