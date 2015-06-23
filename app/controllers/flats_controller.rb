725e0bc7939ed0c54314dc0511fb9682efdb7b19
david muhlberger <david.muhlberger@gmail.com>
2015-06-23 18:43:13 +0200

    navbar button add flat ok

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,6 +1,4 @@
 class FlatsController < ApplicationController
-
-
   def index
     @flats = Flat.where("address_city = :city AND capacity >= :guests", city: params[:city], guests: params[:guests])
   end
@@ -8,5 +6,4 @@ class FlatsController < ApplicationController
   def show
     @flat = Flat.find(params[:id])
   end
-
 end
