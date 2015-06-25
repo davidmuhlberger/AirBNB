068e8e9c1b25fd1e78ec4d03375d0bc4df06f440
noe stauffert <noe.stauffert@gmail.com>
2015-06-25 17:38:50 +0200

    calendar and geoloc

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
+source 'https://rails-assets.org'
 
 ruby '2.2.2'
 
@@ -22,6 +23,14 @@ gem 'figaro'
 
 gem 'omniauth-facebook'
 
+gem "geocoder"
+
+gem 'bootstrap-datepicker-rails'
+
+gem "gmaps4rails"
+
+gem "rails-assets-underscore"
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.1'
 # Use postgresql as the database for Active Record
@@ -38,7 +47,7 @@ gem 'coffee-rails', '~> 4.1.0'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
-gem 'turbolinks'
+# gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,9 +1,21 @@
 class FlatsController < ApplicationController
   def index
-    @flats = Flat.where("LOWER(address_city) = :city AND capacity >= :guests", city: params[:city].downcase, guests: params[:guests])
+    #@flats = Flat.where("LOWER(address_city) = :city AND capacity >= :guests", city: params[:city].downcase, guests: params[:guests])
+    if params[:city].present? && params[:guests].present?
+      @flats = Flat.near(params[:city].downcase, 20).where("capacity >= ?", params[:guests])
+    else
+      @flats = Flat.all
+    end
+    @markers = Gmaps4rails.build_markers(@flats) do |flat, marker|
+      marker.lat flat.latitude
+      marker.lng flat.longitude
+      marker.infowindow "toto"
+      #{}"<button class='btn btn-success'><%= flat.title %></button>"
+    end
   end
 
   def show
     @flat = Flat.find(params[:id])
+    @flat_coordinates = [{ lat: @flat.latitude, lng: @flat.longitude }]
   end
 end
