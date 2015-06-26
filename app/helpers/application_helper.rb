c50638219a3946aa53574887f5b17421d82cdd37
noe stauffert <noe.stauffert@gmail.com>
2015-06-26 13:20:34 +0200

    created notifications

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+
+  def notif_requests
+    sum = 0
+    current_user.flats.each do |flat|
+      sum += flat.bookings.select { |b| b.status == "Pending" }.size
+    end
+    sum
+  end
+
+  def notif_bookings
+    bookings = current_user.bookings.select { |b| b.status == "Pending" }.size
+  end
+
+  def notifications
+    bookings = notif_bookings
+    requests = notif_requests
+    bookings + requests
+  end
+
 end
