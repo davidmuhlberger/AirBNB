0b1afee7a6bf7873d40fbeef8c2f9f3da505afe6
david muhlberger <david.muhlberger@gmail.com>
2015-06-26 16:09:52 +0200

    lookup fixed at 500km

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -2,9 +2,9 @@ class FlatsController < ApplicationController
   def index
     #@flats = Flat.where("LOWER(address_city) = :city AND capacity >= :guests", city: params[:city].downcase, guests: params[:guests])
     if params[:city].present? && params[:guests].present?
-      @flats = Flat.near(params[:city].downcase, 20).where("capacity >= ?", params[:guests])
+      @flats = Flat.near(params[:city].downcase, 500).where("capacity >= ?", params[:guests])
     elsif params[:city].present? && !params[:guests].present?
-      @flats = Flat.near(params[:city].downcase, 20)
+      @flats = Flat.near(params[:city].downcase, 500)
     else
       @flats = Flat.all
     end
